24b8bb1a65d4a89b27856edc4ae36c352da3a4f7
sanket batwal <batwalsanket@gmail.com>
2025-11-16 16:57:14 +0530

    beckend setup

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,26 @@ const creditRoute=require('./routes/credit.route')
  app.use(express.urlencoded({extended:true}))
 
  const cors=require('cors')
- app.use(cors())
- 
+ app.use(
+  cors({
+    origin:[ "http://localhost:5173",//frontend
+           "http://localhost:3000", //backend
+           ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 
  app.use('/api/user',userRoute)
  app.use('/api/expense',expRoute)
  app.use('/api/credit',creditRoute)
+
+app.get("/",(req,res)=>{
+
+   res.send("API Working")
+})
+
 
  app.listen(PORT,HOST,()=>{
 
